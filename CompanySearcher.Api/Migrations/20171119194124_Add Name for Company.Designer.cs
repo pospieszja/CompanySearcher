3b33c19d7290d5454d675f8facda2fc9813b0651
gather <j.pospieszynski@outlook.com>
2017-11-19 20:56:23 +0100

    'Add Name for Company' migration

--- CompanySearcher.Api/Migrations/20171119194124_Add Name for Company.Designer.cs
+++ CompanySearcher.Api/Migrations/20171119194124_Add Name for Company.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CompanySearcher.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171119194124_Add Name for Company")]
+    partial class AddNameforCompany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CompanySearcher.Api/Migrations/20171119194124_Add Name for Company.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CompanySearcher.Api.Migrations
+{
+    public partial class AddNameforCompany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Companies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Companies");
+        }
+    }
+}
