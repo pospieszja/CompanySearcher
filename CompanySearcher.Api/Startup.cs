dcb1dfa0fffd9447224355f6f5ff31b7828f2740
gather <j.pospieszynski@outlook.com>
2017-11-22 23:22:28 +0100

    Enable Cross-Origin Requests

--- CompanySearcher.Api/Startup.cs
+++ CompanySearcher.Api/Startup.cs
@@ -30,6 +30,7 @@ namespace CompanySearcher.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddCors();
             services.AddDbContext<ApplicationDbContext>
                 (options => options.UseSqlServer("Data Source=172.17.0.1,1401;Database=CompanySearcher;User ID=SA;Password=secret1234!"));
 
@@ -45,6 +46,9 @@ namespace CompanySearcher.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:8080").AllowAnyMethod()
+            );
 
             dbInitializer.Initialize();
             app.UseMvc();
