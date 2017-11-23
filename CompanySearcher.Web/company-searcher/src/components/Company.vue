7e8ac5e55ed1777fd826fee0b5742692ee555eca
gather <j.pospieszynski@outlook.com>
2017-11-23 22:32:02 +0100

    Added CompanySearcher.Web project - vue-cli

---
+++ CompanySearcher.Web/company-searcher/src/components/Company.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <p>company</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Company',
+  data () {
+    return {
+    }
+  },
+mounted() {
+    fetch(`http://localhost:5000/api/company/4567KRS`)
+      .then(response => response.json())
+      .then((data) => {
+        this.company = data;
+      })
+ }
+}
+</script>
