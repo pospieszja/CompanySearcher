c3d4e37b964c964a71597d56eca85e6f8bffd992
gather <j.pospieszynski@outlook.com>
2017-11-26 16:45:05 +0100

    Add company result form

--- CompanySearcher.Web/src/App.vue
+++ CompanySearcher.Web/src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <nav class="navbar navbar-light">
+      <a class="navbar-brand" href="#">Company Searcher</a>
+    </nav>
+    <div class="container">
+      <form class="get-company-code">
+        <div class="form-group">
+          <label>KRS/NIP/REGON</label>
+          <input type="text" class="form-control" id="companyCode">
+        </div>
+        <button class="btn btn-primary">Pobierz dane</button>
+      </form>
+      <form>
+        <div class="form-group">
+          <label class="control-label">Nazwa</label>
+          <span class="form-control">Firma</span>
+        </div>
+        <div class="form-group">
+          <div>
+            <label>Ulica</label>
+            <span class="form-control">Ulica</span>
+          </div>
+          <div>
+            <label>Nr</label>
+            <span class="form-control">Nr</span>
+          </div>
+        </div>
+        <div class="form-group">
+          <div>
+            <label>Kod pocztowy</label>
+            <span class="form-control">Kod pocztowy</span>
+          </div>
+          <div>
+            <label>Miasto</label>
+            <span class="form-control">Miasto</span>
+          </div>
+        </div>        
+      </form>
+    </div>    
   </div>
 </template>
 
@@ -24,37 +46,20 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+input {
+  max-width: 300px;
 }
-
-h1, h2 {
-  font-weight: normal;
+.navbar {
+  background-color: #e3f2fd;
+  margin-bottom: 50px;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.get-company-code {
+  margin-bottom: 50px;
 }
 </style>
